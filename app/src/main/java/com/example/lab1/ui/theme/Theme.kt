436d6e2a3a968e131760023fb1783f6a3960961b
Oleksandr Holan <game.play.for.alex@gmail.com>
2025-05-18 00:39:11 +0300

    apply custom colors

--- app/src/main/java/com/example/lab1/ui/theme/Theme.kt
+++ app/src/main/java/com/example/lab1/ui/theme/Theme.kt
@@ -8,25 +8,78 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = AppPrimaryDarkBlue,
+    onPrimary = AppTextOnPrimary,
+    primaryContainer = Color(0xFF004A8F),
+    onPrimaryContainer = Color(0xFFD2E4FF),
+
+    secondary = AppSecondaryGreen,
+    onSecondary = Color.Black,
+    secondaryContainer = Color(0xFF003912),
+    onSecondaryContainer = Color(0xFF97F79D),
+
+    tertiary = AppAccentPink,
+    onTertiary = Color.White,
+    tertiaryContainer = Color(0xFF99003A),
+    onTertiaryContainer = Color(0xFFFFD9E0),
+
+    background = AppBackgroundDark,
+    onBackground = AppTextOnBackgroundDark,
+
+    surface = AppBackgroundDarkElevated,
+    onSurface = AppTextOnBackgroundDark,
+    surfaceVariant = Color(0xFF303030),
+    onSurfaceVariant = Color(0xFFB0B0B0),
+
+    error = AppErrorRed,
+    onError = Color.White,
+    errorContainer = Color(0xFF93000A),
+    onErrorContainer = Color(0xFFFFDAD6),
+
+    outline = Color(0xFF899388)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = AppPrimaryBlue,
+    onPrimary = AppTextOnPrimary,
+    primaryContainer = Color(0xFFD2E4FF),
+    onPrimaryContainer = Color(0xFF001C3B),
+
+    secondary = AppSecondaryGreen,
+    onSecondary = Color.Black,
+    secondaryContainer = Color(0xFF97F79D),
+    onSecondaryContainer = Color(0xFF002105),
+
+    tertiary = AppAccentPink,
+    onTertiary = Color.White,
+    tertiaryContainer = Color(0xFFFFD9E0),
+    onTertiaryContainer = Color(0xFF3E001D),
+
+    background = AppBackgroundLight,
+    onBackground = AppTextOnBackgroundLight,
+
+    surface = AppBackgroundLightElevated,
+    onSurface = AppTextOnBackgroundLight,
+    surfaceVariant = Color(0xFFE0E0E0),
+    onSurfaceVariant = Color(0xFF424242),
+
+    error = AppErrorRed,
+    onError = Color.White,
+    errorContainer = Color(0xFFFFDAD6),
+    onErrorContainer = Color(0xFF410002),
+
+    outline = Color(0xFF73796F)
 )
 
 @Composable
 fun Lab1Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
