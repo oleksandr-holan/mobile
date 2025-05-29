35e76dbb7da3898e094a2af6fbd51e5ecae0335c
Oleksandr Holan <game.play.for.alex@gmail.com>
2025-05-29 12:15:21 +0300

    fix: app name, lint problems

--- app/src/main/java/com/example/lab1/ui/components/AppTopAppBar.kt
+++ app/src/main/java/com/example/lab1/ui/components/AppTopAppBar.kt
@@ -3,11 +3,8 @@ package com.example.lab1.ui.components
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lab1.ui.theme.Lab1Theme
 import androidx.compose.ui.res.stringResource
 import com.example.lab1.R
 
